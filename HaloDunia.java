19ceda447cdd664cac733cf72939b6a128901c9e
Pangestu Nugraha <49370353+wizjdan@users.noreply.github.com>
2023-07-20 19:50:14 +0700

    Add files via upload

---
+++ HaloDunia.java
@@ -0,0 +1,8 @@
+public class HaloDunia {
+    public static void main(String[] args) {
+        System.out.println("Halo Selamat Datang");
+        System.out.println("Perkenalkan Nama Saya Pangestu Nugraha");
+        System.out.println("Dan ini adalah dokumentasi pembelajaran saya tentang bahasa pemrograman Java");
+        System.out.println("Terimakasih");
+    }
+}
